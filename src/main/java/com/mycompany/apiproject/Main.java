a9902d7615a9178d14896f28124f03fe4d6b2a7a
erquier <erquier@erquier>
2018-07-05 19:41:53 -0400

    Parte del cliente api

---
+++ src/main/java/com/mycompany/apiproject/Main.java
@@ -0,0 +1,35 @@
+package com.mycompany.apiproject;
+import static spark.Spark.*;
+import com.google.gson.Gson;
+import java.util.ArrayList;
+import java.util.Date;
+import static spark.Spark.*;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import spark.ModelAndView;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import spark.Filter;
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        port(1234);
+        staticFiles.location("/public");
+        
+        get("/hello", (req, res) -> "Hello World");
+        
+        
+        get("/cliente", (req, res)->{
+            res.redirect("index.html");
+            return null;
+        
+        });
+    }
+}
